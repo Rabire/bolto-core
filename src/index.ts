b65a986fc02d33690b706aa694bd984dd9a6bc9c
rabire <rabireh@outlook.fr>
2026-05-15 17:57:08 +0200

    prisma setup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 import startApolloServer from "./infrastructure/graphql";
+import { prisma } from "./infrastructure/prisma/client";
 
-startApolloServer();
+async function main() {
+  startApolloServer();
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
