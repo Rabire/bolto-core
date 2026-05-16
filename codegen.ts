09b21544a7f558cdc95c8d9c59bc128088825ccc
rabire <rabireh@outlook.fr>
2026-05-16 16:48:54 +0200

    feat: add Date scalar definition and update type mappings in GraphQL schema

--- codegen.ts
+++ codegen.ts
@@ -7,8 +7,11 @@ const config: CodegenConfig = {
       plugins: ["typescript", "typescript-resolvers"],
       config: {
         useTypeImports: true,
+        strictScalars: true,
+        enumsAsTypes: true,
         scalars: {
           ID: "string",
+          Date: "Date",
         },
         mappers: {
           User: "@iam/app/user.dto#UserDto",
